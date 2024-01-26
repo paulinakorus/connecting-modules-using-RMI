c128f82b4321af4eb89383ae3d068c40cb02e924
Paulina Korus <paukor2606@gmail.com>
2024-01-26 19:41:51 +0100

    Changing name from Lab6 to Lab7

---
+++ service/src/main/java/org/example/service/model/OrderOld.java
@@ -0,0 +1,45 @@
+package org.example.service.model;
+
+import org.example.service.model.enums.OrderStatus;
+
+import java.util.List;
+import java.util.UUID;
+
+public class OrderOld {
+    private UUID orderID = UUID.randomUUID();
+    private UUID userID;
+    private List<Product> productList;
+    private OrderStatus orderStatus;
+
+    public UUID getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(UUID orderID) {
+        this.orderID = orderID;
+    }
+
+    public UUID getUserID() {
+        return userID;
+    }
+
+    public void setUserID(UUID userID) {
+        this.userID = userID;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+}
